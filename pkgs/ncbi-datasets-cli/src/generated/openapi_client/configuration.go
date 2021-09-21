7046c2474a9ae66541a136d7b8469ad047d06326
Brad Holmes <holmesbr@ncbi.nlm.nih.gov>
2021-09-21 09:49:48 -0400

    Release updated docs for 12.13.2

--- pkgs/ncbi-datasets-cli/src/generated/openapi_client/configuration.go
+++ pkgs/ncbi-datasets-cli/src/generated/openapi_client/configuration.go
@@ -101,7 +101,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/12.13.1/go",
+		UserAgent:        "OpenAPI-Generator/12.13.2/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
