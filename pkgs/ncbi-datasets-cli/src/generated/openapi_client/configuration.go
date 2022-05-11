08bd4006a139515d3c4f2a2b6cb69aacec0e0978
RAPT-release <rapt@ncbi>
2022-05-11 06:53:34 -0400

    Release updated docs for 13.18.2

--- pkgs/ncbi-datasets-cli/src/generated/openapi_client/configuration.go
+++ pkgs/ncbi-datasets-cli/src/generated/openapi_client/configuration.go
@@ -101,7 +101,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/13.18.0/go",
+		UserAgent:        "OpenAPI-Generator/13.18.2/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
