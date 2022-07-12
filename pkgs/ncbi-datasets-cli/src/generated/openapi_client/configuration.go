7789a7361c9405a67f3f75c32a24f3fcfa542b89
Tomcat service account <tomcat@tcbuilder22.be-md.ncbi.nlm.nih.gov>
2022-07-12 15:46:52 -0400

    Release updated docs for 13.28.2

--- pkgs/ncbi-datasets-cli/src/generated/openapi_client/configuration.go
+++ pkgs/ncbi-datasets-cli/src/generated/openapi_client/configuration.go
@@ -101,7 +101,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/13.28.1/go",
+		UserAgent:        "OpenAPI-Generator/13.28.2/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
